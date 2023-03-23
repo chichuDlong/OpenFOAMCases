8b099114be70687571c441e0e75eaa3a252765a0
shuoyue <1162288059@qq.com>
2023-03-23 19:12:09 +0800

    topoSet createPatch

---
+++ 005_bubble/run.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+cd ${0%/*} || exit 1    # Run from this directory
+
+# Source tutorial run functions
+. $WM_PROJECT_DIR/bin/tools/RunFunctions
+
+# Set application name
+application=$(getApplication)
+
+#gmsh -3 Mesh1D.geo
+#gmshToFoam Mesh1D.msh
+#changeDictionary
+
+blockMesh
+
+topoSet
+
+createPatch -overwrite
+
+setFields
+
+runApplication $application
+
